3349204ebb5b835825003b8e0cd6fd1ae26d958e
Romain Brault <mail@romainbrault.com>
2024-11-16 04:16:36 +0000

    feat: ✨ add command line interface

---
+++ src/test_pypi_linux_py311_test/cli/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# SPDX-FileCopyrightText: © 2024 Romain Brault <mail@romainbrault.com>
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+"""Top-level executable."""
+
+from test_pypi_linux_py311_test.cli.entrypoint import (
+    test_pypi_linux_py311_test,
+)
+
+
+test_pypi_linux_py311_test()
